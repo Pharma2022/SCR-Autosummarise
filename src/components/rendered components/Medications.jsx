6ab19e866d77c22f9d55b460a3328b137ae96154
shah aowal <kashim91@live.com>
2023-05-10 08:17:10 +0000

    added ordering to reg meds if added manually and not copied and pasted from SCR

--- src/components/rendered components/Medications.jsx
+++ src/components/rendered components/Medications.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import { useFormContext } from '../../context/formContext'
+import { nanoid } from 'nanoid'
 
 const Medications = () => {
     const {form:{ hasAllergy,allergies,hasRegMeds,hasAcuteMeds,hasOtcMeds,otcMeds},renderedAcuteMeds,renderedRegMeds
 
 }=useFormContext()
 
+console.log (renderedRegMeds.split('\n'))
+
   return (
     <div>
         <p className='bold underline'>Allergies:</p>
         <p>Allergies: {hasAllergy? allergies:"NKDA"}</p>
         <p className='bold underline'>Regular Mediation</p> 
-        <p>{hasRegMeds?         renderedRegMeds:'Nil' }</p>
+        { hasRegMeds? 
+        <ol>
+          {renderedRegMeds.split('\n').map(med=>(<li key={nanoid()}>{med} </li>))}
+        </ol>
+        :     <p>Nil</p>}
         <p className='bold underline'>Acute Medication</p>  
         <p>{hasAcuteMeds?     renderedAcuteMeds:'Nil' }</p>
         <p className='bold underline'>OTC Medication</p>    

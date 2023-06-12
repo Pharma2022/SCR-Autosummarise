e2d6784eefa8787e8549bdebf7daee549cf4401d
shah aowal <kashim91@live.com>
2023-06-12 20:09:25 +0000

    fixed redirects, styles

--- src/components/contact components/ContactItemProperty.jsx
+++ src/components/contact components/ContactItemProperty.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const ContactItemProperty=({copy,isCopied,text,type})=>(  <span className='flex-row copy-container'>
-<p onClick={copy} className="email ">
+<p onClick={copy} className="email" style={{wordBreak:'break-all'}}>
  <span className='bold type pointer-no-underline'>{type}: </span> <span className='pointer'>{text}</span>
-{isCopied&& <span className='copy-button'>Copied!
+{isCopied&& <span className='copy-button' style={{wordBreak:'keep-all'}}>Copied!
 </span>}
 </p>
 
